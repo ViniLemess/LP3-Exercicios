ea2aa26728c6e6240ad73c70b19ba7b1976ebb29
ViniLemess <viniciuslemescaetano@gmail.com>
2022-05-13 00:16:59 -0300

    Validacao dos campos de Customer

--- exercicio-avaliativo-4/src/main/java/com/vinilemess/metallica/model/Customer.java
+++ exercicio-avaliativo-4/src/main/java/com/vinilemess/metallica/model/Customer.java
@@ -2,17 +2,22 @@ package com.vinilemess.metallica.model;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Customer {
 
     private String name;
     private LocalDate birthDate;
-    private boolean student;
+    private Boolean student;
 
-    public Customer(String name, LocalDate birthDate, boolean student) {
+    public Customer(String name, LocalDate birthDate, Boolean student) {
         this.name = name;
         this.birthDate = birthDate;
         this.student = student;
+        isValid();
     }
 
     public String getName() {
@@ -30,5 +35,19 @@ public class Customer {
     public boolean isElderly() {
         long age = ChronoUnit.DAYS.between(LocalDate.now(), birthDate);
         return age >= 60;
+    }
+
+    private void isValid() {
+        List<String> messages = new ArrayList<>();
+        if (this.name == null || this.name.isBlank()) {
+            messages.add("Invalid name!");
+        }
+        if (this.birthDate == null) {
+            messages.add("birth date cannot be null!");
+        }
+        if (this.student == null) {
+            messages.add("Student field must be informed!");
+        }
+        throw new IllegalArgumentException(messages.toString());
     }
 }
