0ace0a9ce7fe03a5ae31aabca1775cee02af3739
ViniLemess <viniciuslemescaetano@gmail.com>
2022-05-22 20:15:57 -0300

    Remove imports nao usados

--- exercicio-avaliativo-4/src/main/java/com/vinilemess/metallica/model/Customer.java
+++ exercicio-avaliativo-4/src/main/java/com/vinilemess/metallica/model/Customer.java
@@ -3,9 +3,7 @@ package com.vinilemess.metallica.model;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Customer {
 
